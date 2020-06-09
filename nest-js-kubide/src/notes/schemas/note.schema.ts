76c6835e0a4048c8a62e9b88342b00052c21a690
Andres Atehortua <anderson17colombia@hotmail.com>
2020-06-09 16:32:53 +0200

    [ADD] Timestamps added

--- nest-js-kubide/src/notes/schemas/note.schema.ts
+++ nest-js-kubide/src/notes/schemas/note.schema.ts
@@ -1,7 +1,12 @@
 import { Schema } from 'mongoose';
 
-export const NoteSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  favorite: { type: Boolean, enum: [true, false], default: false },
-});
+export const NoteSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    favorite: { type: Boolean, enum: [true, false], default: false },
+  },
+  {
+    timestamps: true,
+  },
+);
