224ecbf332e79115f4c5be471bb48873af6764fd
Andres Atehortua <anderson17colombia@hotmail.com>
2020-06-12 14:23:20 +0200

    [FIX] Corregido tipado

--- nest-js-kubide/src/app.service.ts
+++ nest-js-kubide/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): Object {
+  getHello(): any {
     return { message: 'Welcome!', status: 200, ok: true };
   }
 }
